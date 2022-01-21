3375a330a042eaed9475a9cc3b3fc1f3720386ed
JIn Zhou <GinChow@users.noreply.github.com>
2022-01-21 14:28:31 +0800

    update 145.binary-tree-postorder-traversal.py

---
+++ 145.binary-tree-postorder-traversal.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode.cn id=145 lang=python3
+#
+# [145] 二叉树的后序遍历
+#
+# https://leetcode-cn.com/problems/binary-tree-postorder-traversal/description/
+#
+# algorithms
+# Easy (75.31%)
+# Total Accepted:    353.8K
+# Total Submissions: 469.7K
+# Testcase Example:  '[1,null,2,3]'
+#
+# 给你一棵二叉树的根节点 root ，返回其节点值的 后序遍历 。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：root = [1,null,2,3]
+# 输出：[3,2,1]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：root = []
+# 输出：[]
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：root = [1]
+# 输出：[1]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 树中节点的数目在范围 [0, 100] 内
+# -100 <= Node.val <= 100
+# 
+# 
+# 
+# 
+# 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+        result = []
+        def dfs(root):
+            if not root: return
+            dfs(root.left)
+            dfs(root.right)
+            result.append(root.val)
+        dfs(root)
+
+        return result
+                
+                
